fec60e36be04b425e32d2e8071e60c7f3ae97143
enlegato <enlegato@csuchico.edu>
2024-11-13 11:27:12 -0800

    initial commit

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -5,11 +5,11 @@
 #include <gtest/gtest.h>
 #include "Password.h"
 
-class PasswordTest : public ::testing::Test
+class PracticeTest : public ::testing::Test
 {
 	protected:
-		PasswordTest(){} //constructor runs before each test
-		virtual ~PasswordTest(){} //destructor cleans up after tests
+		PracticeTest(){} //constructor runs before each test
+		virtual ~PracticeTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
@@ -18,5 +18,32 @@ TEST(PasswordTest, single_letter_password)
 {
 	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, no_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, only_letters_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("ZdkSkenSiQPgn");
+	ASSERT_EQ(13, actual);
+}
+
+TEST(PasswordTest, only_numbers_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("0138941784");
+	ASSERT_EQ(10, actual);
+}
+TEST(PasswordTest, single_number_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("0");
 	ASSERT_EQ(1, actual);
 }
